e13e47d1ff2e3d07424cf4f89c8dd6ee06726013
Anietie-Edet <aedet430@gmail.com>
2023-03-16 16:24:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - program that prints to standard error
+ * main - point of entry
  * Return: 1 (success)
  */
 
 int main(void)
 {
-       	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+       	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
         return (1);
 }
