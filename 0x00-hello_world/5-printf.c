75e044d84204b138249afbb34f036fc13fdeb786
Anietie-Edet <aedet430@gmail.com>
2023-03-16 11:49:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/**                                                    *  main - this is the printf function to print formatted text
- * 
+/**                                                    *  main - this is the printf function
+ *
  *  Return: 0 when successful
  */
 
 int main(void)                                        {
-        printf("with proper grammar, but the outcome is a piece of art\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
