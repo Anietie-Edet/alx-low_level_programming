c637f94af682242a7a05660064f2b6bf172e0764
Anietie-Edet <aedet430@gmail.com>
2023-03-16 15:33:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
-{ #include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints to standard error
+ * Return: 1 (success)
+ */
 
 int main(void)
 {
        	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-
         return (1);
-} }
+}
