9b6e96d5436bc7f68a40f491edf8f603743ee692
Anietie-Edet <aedet430@gmail.com>
2023-03-21 17:01:01 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ *
+ * @f: parameter 1
+ * @k: parameter 2
+ *
+ * Return: sum
+ */
+
+int add(int f, int k)
+{
+	return (f + k);
+}
