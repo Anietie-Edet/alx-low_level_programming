4312ef1bd5e904c209893f9c4a39e91b8eb864e7
Anietie-Edet <aedet430@gmail.com>
2023-04-11 10:17:24 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * _strdup - returns a pointer to a newly allocated space in memory
@@ -29,7 +30,7 @@ char *_strdup(char *str)
 	while (str[length])
 		length++;
 	/* allocate memory for the new string (+1 for null terminator*/
-	replica = malloc(sizeof(char) + 1);
+	replica = malloc(sizeof(char) * (length + 1));
 	/* check of malloc failed to allocate memory*/
 	if (replica == NULL)
 		return (NULL);
