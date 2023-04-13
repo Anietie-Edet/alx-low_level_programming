07584acb175acd95070c11982ebb9c333a6c66a9
Anietie-Edet <aedet430@gmail.com>
2023-04-13 05:25:54 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -35,8 +36,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (c = 0; c < a; c++)
 		s[c] = s1[c];
+
 	for (c = 0; c < b; c++)
 		s[c + a] = s2[c];
+
 	s[a + b] = '\0';
 	return (s);
 }
