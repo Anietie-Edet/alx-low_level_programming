9f274356ff2cf831544db99e93ceff9cac7c0cd6
Anietie-Edet <aedet430@gmail.com>
2023-04-13 15:37:47 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,13 +56,13 @@ void errors(void)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: always 0 (Success)
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 		char *s1, *s2;
-		int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+		int len1, len2, len, i, gg, digit1, digit2, *result, a = 0;
 
 		s1 = argv[1], s2 = argv[2];
 		if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -78,16 +78,16 @@ int main(int argc, char *argv[])
 		for (len1 = len1 - 1; len1 >= 0; len1--)
 		{
 			digit1 = s1[len1] - '0';
-			carry = 0;
+			gg = 0;
 			for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 			{
 				digit2 = s2[len2] - '0';
-				carry += result[len1 + len2 + 1] + (digit1 * digit2);
-				result[len1 + len2 + 1] = carry % 10;
-				carry /= 10;
+				gg += result[len1 + len2 + 1] + (digit1 * digit2);
+				result[len1 + len2 + 1] = gg % 10;
+				gg /= 10;
 			}
-			if (carry > 0)
-				result[len1 + len2 + 1] += carry;
+			if (gg > 0)
+				result[len1 + len2 + 1] += gg;
 		}
 		for (i = 0; i < len - 1; i++)
 		{
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
 		}
 		if (!a)
 			_putchar('0');
-		_putchar('\n');
+		printf("\n");
 		free(result);
 		return (0);
 	}
