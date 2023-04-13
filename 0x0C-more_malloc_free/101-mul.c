eda9229d5aeb59af41fb896861012f0f4d445d02
Anietie-Edet <aedet430@gmail.com>
2023-04-13 08:27:25 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	{
 		for (b = 0; argv[a][b] != '\0'; b++)
 		{
-			if (argv[a][b] > 57 || argv[a][b] < 48)
+			if (argv[a][b] > '9' || argv[a][b] < '0')
 			{
 				printf("Error\n");
 					exit(98);
